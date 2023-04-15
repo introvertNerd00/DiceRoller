f38b673d4215b0e76e2aac89a65697fd1e0fd8c9
Hamza <hamza.afzalkhan9@gmail.com>
2023-04-15 20:24:02 +0500

    added background color

--- app/src/main/java/com/edex/diceroller/MainActivity.kt
+++ app/src/main/java/com/edex/diceroller/MainActivity.kt
@@ -4,20 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.edex.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,10 +47,14 @@ fun DiceRollingApp(){
         .wrapContentSize(align = Alignment.Center))
 }
 
+
+
 @Composable
 fun DiceRolling(modifier: Modifier= Modifier){
-    var rolling =1
-    val ImageResource = when(rolling){
+    var rolling by remember {
+        mutableStateOf(1)
+    }
+    val imageResource = when(rolling){
         1 -> R.drawable.dice_1
         2 -> R.drawable.dice_2
         3 -> R.drawable.dice_3
@@ -51,12 +62,17 @@ fun DiceRolling(modifier: Modifier= Modifier){
         5 -> R.drawable.dice_5
         else -> R.drawable.dice_6
     }
-Column(modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally) {
+Column(modifier = modifier
+        .fillMaxSize()
+        .background(color = Color.Cyan),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+        ) {
 
+    Text(text = stringResource(R.string.dice_rolling_app) )
 
-    Image(painter = painterResource(id = ImageResource), contentDescription =rolling.toString() )
-
+    Image(painter = painterResource(id = imageResource), contentDescription =rolling.toString() )
+    Spacer(modifier = Modifier.height(24.dp))
     Button(onClick = { rolling= (1..6).random()}) {
         Text(text = stringResource(id = R.string.roll_button))
     }
