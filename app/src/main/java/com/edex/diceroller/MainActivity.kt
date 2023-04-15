2adc75d8883a39d2e9f34f683d5da7826dbef764
Hamza <hamza.afzalkhan9@gmail.com>
2023-04-15 15:41:06 +0500

    added dice Images

--- app/src/main/java/com/edex/diceroller/MainActivity.kt
+++ app/src/main/java/com/edex/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.edex.diceroller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
@@ -13,6 +14,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.edex.diceroller.ui.theme.DiceRollerTheme
@@ -39,9 +42,22 @@ fun DiceRollingApp(){
 
 @Composable
 fun DiceRolling(modifier: Modifier= Modifier){
+    var rolling =1
+    val ImageResource = when(rolling){
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
 Column(modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally) {
-    Button(onClick = { /*TODO*/ }) {
+
+
+    Image(painter = painterResource(id = ImageResource), contentDescription =rolling.toString() )
+
+    Button(onClick = { rolling= (1..6).random()}) {
         Text(text = stringResource(id = R.string.roll_button))
     }
 
